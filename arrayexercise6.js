cdcf95973533753bf7eef025be1e3c3e3071d918
Wei <vv880923@gmail.com>
2018-05-29 16:01:50 -0700

    js array exercise

---
+++ arrayexercise6.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2018-05-29 15:53:00
+ * @version $Id$
+ */
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+console.log(array.shift());
+
+console.log(array.sort());
+
+console.log(array.push("Kiwi"));
+
+console.log(array.splice(1,1));
+
+console.log(array.reverse())

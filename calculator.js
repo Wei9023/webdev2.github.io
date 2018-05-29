8e5e5c840ad24c3e3ff5c51a041e7fd42de17c86
Wei <vv880923@gmail.com>
2018-05-28 23:55:52 -0700

    calculator v1

---
+++ calculator.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2018-05-28 23:35:33
+ * @version $Id$
+ */
+
+console.log("hello");
+var firstNum = document.getElementById(n1);
+var secondNum = document.getElementById(n2);
+
+function plus(firstNum, secondNum){
+	console.log(1);
+	alert(Number(firstNum) + Number(secondNum));
+}
+
+function subtract(firstNum, secondNum){
+	console.log(2);
+	alert(Number(firstNum) - Number(secondNum));
+}
+
+function multiply(firstNum, secondNum){
+	console.log(3);
+	alert(Number(firstNum) * Number(secondNum));
+}
+
+function devide(firstNum, secondNum){
+	console.log(4);
+	alert(Number(firstNum) / Number(secondNum));
+}

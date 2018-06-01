504ef1aecec3f08f0bc43d728c0d50069c52a90d
Wei <vv880923@gmail.com>
2018-05-31 20:02:47 -0700

    modified shopping list

--- shoppinglist.js
+++ shoppinglist.js
@@ -5,18 +5,38 @@
  * @version $Id$
  */
 
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+//check off specific todos by clicking
+$("ul").on("click", "li" , function(){
+/*	if($(this).css("color") === "rgb(128, 128, 128)"){
+		$(this).css("color", "rgb(0,0,0)");
+		$(this).css("text-decoration", "NONE");
+	}
+	else{
+		$(this).css("color", "gray");
+		$(this).css("text-decoration", "line-through");
+	} */
+	$(this).toggleClass("completed");	
+})
 
-button.addEventListener("click", function(){
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendchild("li");
-	input.value="";
+$("ul").on("click", "span", function(){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+})
+
+$("input[type='text']").keypress(function(event){
+	if(event.which ===13){
+//		grabbing new todo text from input
+		var todoText = $(this).val();
+		//$(this).val("");
+		//creat a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span>" + todoText + "</li>");
+		console.log($("input[type='text']").value);
+	}
+	$("#inp").value = "";
 });
 
-var buttonDone = document.getElementsByClassName("buttonDone");
-done.addEventListener("click", function(){
-	li.classList.toggleclass("done");
+$(".fa-plus").click(function(){
+	$("input[type='text'").fadeToggle();
 })
